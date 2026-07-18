e49c13aa7009268977ebfb3886a68190ae9fdfd9
ahmedmarhoum31 <marhoum80@yahoo.com>
2026-07-18 13:08:44 +0100

    Create getComputerChoice() function to return either rock,paper or scissor
    - Add getComputerChoice() to generate the computer's move
    - Use Math.random() to select a random choice
    - Return either rock, paper, or scissor based on the generated number

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+// Create a function that return either rock, paper, or scissors
+function getComputerChoice()
+{
+    //create a float variable for randomNumber
+    let randomNumber = Math.random() * 3;
+    //Make a conditional statement to choose and return either rock, paper ,scissors based on the random number.
+    if(randomNumber >=0 && randomNumber <1)
+    {
+        return "rock";
+    }
+    else if(randomNumber >=1 && randomNumber <2)
+    {
+        return "paper";
+    }
+    else if(randomNumber >=2 && randomNumber<3)
+    {
+        return "scissor";
+    }
+}
+
